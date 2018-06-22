99e12d5fc323335a2df2bca07faedd3b944daa6e
Lindsay Sack <lindsay.sack@gmail.com>
2018-06-22 13:09:17 -0400

    started to update agreement with Swap info

--- contracts/SwapAgreement.sol
+++ contracts/SwapAgreement.sol
@@ -1,10 +1,11 @@
 pragma solidity ^0.4.17;
 
-contract Adoption {
-    address[16] public adopters;
+contract SwapAgreement {
+    address initiator; 
+    address respondent;
 
 // Adopting a pet
-function adopt(uint petId) public returns (uint) {
+function finalizeSwap(uint petId) public returns (uint) {
   require(petId >= 0 && petId <= 15);
 
   adopters[petId] = msg.sender;
