332c3a7beb882b6779ee67f1296a7f37ad1453f5
Lindsay Sack <lindsay.sack@gmail.com>
2018-06-22 16:29:41 -0400

    Updated contract setting initiators and respondents

--- contracts/SwapAgreement.sol
+++ contracts/SwapAgreement.sol
@@ -1,20 +1,34 @@
+// Requires Solidity version 0.4.17 or above in order to run
 pragma solidity ^0.4.17;
 
 contract SwapAgreement {
-    address initiator; 
-    address respondent;
 
-// Adopting a pet
-function finalizeSwap(uint petId) public returns (uint) {
-  require(petId >= 0 && petId <= 15);
+  // Declares variables that will be used to store addresses for each party involved in swap
+    event SwapLog(
+        address initiator,
+        address respondent,
+        uint256 id
+    );
 
-  adopters[petId] = msg.sender;
+    Swap[] swaps;
 
-  return petId;
-}
+    struct Swap {
+        address initiator;
+        address respondent;
+    }
 
-// Retrieving the adopters
-function getAdopters() public view returns (address[16]) {
-  return adopters;
-}
+// Set the address for the person who initiates the swap
+    function initiatedSwap() public returns (uint256) {
+        uint256 id = swaps.push(Swap(msg.sender, 0x0000)) - 1; 
+        emit SwapLog(msg.sender, 0x0000, id);
+        return id;
+    }
+
+// Set the address for the person who accepts the swap.
+    function finalizeSwap(uint256 id) public {
+        Swap storage swap = swaps[id];
+        require(msg.sender != swap.initiator); // Prevent setting same address to initiator AND respondent
+        swap.respondent = msg.sender; 
+        emit SwapLog(swap.initiator, swap.respondent, id);
+    }
 }
